38dadbd76fcaeb7563bb4fda1007a91142476ac3
hellokaibot-alt <hellokaibot@gmail.com>
2026-02-13 09:42:04 +0000

    Add real-time chat polling - auto-refresh every 3 seconds with new message indicator

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ const syncMessagesFromSessions = async () => {
   }
 };
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
